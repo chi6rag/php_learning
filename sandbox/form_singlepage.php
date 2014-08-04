a5d5985365739e6ba9ed8f4184b6a0a75fd52959
root <root@chi6rag-K56CA.(none)>
2014-08-04 19:07:44 +0530

    updte

--- sandbox/form_singlepage.php
+++ sandbox/form_singlepage.php
@@ -1,3 +1,17 @@
+<?php
+	require("sources/header_functions.php");
+	if ( isset($_POST["Submit"]) ){
+		$username = $_POST["username"];
+		$password = $_POST["password"];
+		if( $username=="chi6rag" && $password=="hello"){
+			redirect_to("assoc_array.php");
+		}
+		$message = "There were some errors...";
+	}else{
+		 $message = "There were some errors...";
+		 $username = "";
+	}
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -5,34 +19,48 @@
 		<style>
 		body {
 			background-color: #A30000;
+			color: white;
 		}
 		input {
-			height: 60px;
-			width: 240px;
+			height: 50px;
+			width: 450px;
 			border: 0px solid;
-			padding: 5px;
+			padding: 20px;
 			font-family: sans-serif;
 			margin: 0 auto;
 			font-size: 24px;
 			font-weight: 0;
 			color: gray;
+			border-radius: 2px;
 		}
 		#submit input{
-			height: 30px;
+			
 			width: 120px;
-			font-size: 12px;
+			font-size: 20px;
+			text-align: center;
+			vertical-align: middle;
+			line-height: 10px;
 		}
 		.submit-form {
-			margin-top: 200px;
+			margin-top: 150px;
 			text-align: center;
+			padding: 20px;ame] => [password] =>
+
+		}
+		p{
+			color: white;
+			font-family: sans-serif;
 		}
 
 		</style>
 	</head>
 	<body>
 		<div class="submit-form">
-			<form action="processing.php" method="post" >
-				<input type="text" name="username" placeholder="Email" /><br /><br />
+		<?php
+			echo "{$message}<br />";
+		?>
+			<form action="form_singlepage.php" method="post" >
+				<input type="text" name="username" placeholder="Email" value="<?php echo $username; ?>" /><br /><br />
 				<input type="password" name="password" placeholder="Password" /><br /><br />
 				<span id="submit"><input type="submit" name="Submit" /></span>
 			</form>
