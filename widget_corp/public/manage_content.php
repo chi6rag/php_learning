4a8ba09d7f89c519c2144cc282fa1ffc07b49cfa
root <root@chi6rag-K56CA.(none)>
2014-08-11 01:32:04 +0530

    Update manage content for subjects and pages

--- widget_corp/public/manage_content.php
+++ widget_corp/public/manage_content.php
@@ -1,34 +1,39 @@
 <?php
-	// 1. Connecting to the database
-	$host = "localhost";
-	$username = "widget_cms";
-	$password = "secretpassword";
-	$db_name = "widget_corp";
-	$connection = mysqli_connect($host, $username, $password, $db_name);
-
-	// Testing connection
-	if( mysqli_connect_errno($connection) ){
-		die("Database query failed");
-	}
+	require_once("../includes/database_connection.php");
+	require_once("../includes/functions.php");
 ?>
 <?php include("../includes/layout/header.php"); ?>
-<?php
-	// 3. Firing query and preparing resource
-	$query = "SELECT * FROM subjects;";
-	$resource = mysqli_query($connection, $query);
-?>
 <div class="main">
 	<div class="navigation">
 		<ul class="subjects">
-			<?php // Operating on $?>
-			<?php while( $subjects_assoc = mysqli_fetch_assoc($resource)){ ?>
-			<li>
-				<?php
-					echo "{$subjects_assoc["menu_name"]}" . "({$subjects_assoc["id"]})";
-				}
+			<?php
+				// 2.a Firing query and preparing resource_subjects
+				$query = "SELECT * FROM subjects WHERE visible=1;";
+				$resource_subjects = mysqli_query($connection, $query);
+				mysqli_check_resource($resource_subjects);
+			?>
+			<?php 	  // 3.a Fetching resource_subjects and using information ?>
+			<?php     while( $subjects_assoc = mysqli_fetch_assoc($resource_subjects) ){ ?>
+			<li><?php 		echo "{$subjects_assoc["menu_name"]}";
+							// 2.b Firing query and preparing resource_pages
+							$query = "SELECT * FROM pages WHERE subject_id={$subjects_assoc["id"]};";
+							$resource_pages = mysqli_query($connection, $query);
 				?>
+				<ul class="pages">
+					<?php 		//3.b Fetching resource_pages and using information ?>
+					<?php 		while( $pages_assoc = mysqli_fetch_assoc($resource_pages) ){ ?>
+					<li><?php 		echo "{$pages_assoc["menu_name"]}"; ?></li>
+					<?php 		} ?>
+				</ul>
 			</li>
+			<?php 	  } ?>
 		</ul>
+		<?php
+			// 4.a Free Result
+			mysqli_free_result($resource_subjects);
+			// 4.b Free Result
+			mysqli_free_result($resource_pages);
+		?>
 	</div>
 	<div class="page">
 		<h2>Manage Content</h2>
