45e8e4a064d6f5aab5eea0dbb7685b3275295eea
root <root@chi6rag-K56CA.(none)>
2014-08-11 03:31:54 +0530

    Refactor navigation in manage_content.php - tough task - beware

--- widget_corp/includes/functions.php
+++ widget_corp/includes/functions.php
@@ -19,4 +19,46 @@
 		$resource_pages = mysqli_query($connection, $query);
 		return $resource_pages;
 	}
+
+	function navigation($selected_subject_id, $selected_page_id){
+		$output = "<ul class=\"subjects\">";
+		// 2.a Firing query and preparing resource_subjects
+		$resource_subjects = get_all_subjects();
+		// 3.a Fetching resource_subjects and using information
+		while( $subjects_assoc = mysqli_fetch_assoc($resource_subjects) ){
+			$output .= "<li ";
+			if( $subjects_assoc["id"] == $selected_subject_id ){
+				$output .= "class=\"selected\"";
+			}
+			$output .= ">";
+			$output .= "<a href=\"manage_content.php?subject=";
+			$output .= urlencode($subjects_assoc["id"]);
+			$output .= "\">";
+			$output .= $subjects_assoc["menu_name"];
+			$output .= "</a>";
+			
+			// 2.b Firing query and preparing resource_pages
+			$resource_pages = get_pages_by_subject_id($subjects_assoc["id"]);
+			$output .= "<ul class=\"pages\">";
+			//3.b Fetching resource_pages and using information
+			while( $pages_assoc = mysqli_fetch_assoc($resource_pages) ){ 
+				$output .= "<li ";
+				if( $selected_page_id == $pages_assoc["id"]){
+					$output .= "class=\"selected\"";
+				}
+				$output .= ">";
+				$output .= "<a href=\"manage_content.php?page=";
+				$output .= urlencode($pages_assoc["id"]);
+				$output .= "\">";
+				$output .= $pages_assoc["menu_name"];
+				$output .= "</a></li>";
+			}
+			$output .= "</ul></li>";
+		}
+		$output .= "</ul>";
+		mysqli_free_result($resource_subjects);
+		mysqli_free_result($resource_pages);
+		return $output;
+	}
+
 ?>
