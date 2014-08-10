5878ddd439aa28d7579265ae506cc3d18e3aed81
root <root@chi6rag-K56CA.(none)>
2014-08-11 02:38:46 +0530

    Include highlighting feature in manage_content.php

--- widget_corp/public/manage_content.php
+++ widget_corp/public/manage_content.php
@@ -4,14 +4,14 @@
 ?>
 <?php
 	if( isset($_GET["subject"]) ){
-		$subject_id = $_GET["subject"];
-		$page_id = null;
+		$selected_subject_id = $_GET["subject"];
+		$selected_page_id = null;
 	}elseif( isset($_GET["page"]) ){
-		$subject_id = null;
-		$page_id = $_GET["page"];
+		$selected_subject_id = null;
+		$selected_page_id = $_GET["page"];
 	}else{
-		$subject_id = null;
-		$page_id = null;
+		$selected_subject_id = null;
+		$selected_page_id = null;
 	}
 ?>
 <?php include("../includes/layout/header.php"); ?>
@@ -23,19 +23,37 @@
 				$resource_subjects = get_all_subjects();
 			?>
 			<?php 	  // 3.a Fetching resource_subjects and using information ?>
-			<?php 		while( $subjects_assoc = mysqli_fetch_assoc($resource_subjects) ){ ?>
-			<li>    		<a href="manage_content.php?subject=<?php echo $subjects_assoc["id"]?>"><?php 	echo "{$subjects_assoc["menu_name"]}"; ?></a>
-							<?php 	// 2.b Firing query and preparing resource_pages
-									$resource_pages = get_pages_by_subject_id($subjects_assoc["id"]);
+			<?php
+				while( $subjects_assoc = mysqli_fetch_assoc($resource_subjects) ){ ?>
+					<?php
+						echo "<li ";
+						if( $subjects_assoc["id"] == $selected_subject_id ){
+							echo "class=\"selected\"";
+						}
+						echo ">";
+					?>
+						<a href="manage_content.php?subject=<?php echo urlencode($subjects_assoc["id"]); ?>">
+								<?php echo "{$subjects_assoc["menu_name"]}"; ?></a>
+						<?php 	// 2.b Firing query and preparing resource_pages
+								$resource_pages = get_pages_by_subject_id($subjects_assoc["id"]);
+						?>
+						<ul class="pages">
+							<?php 	//3.b Fetching resource_pages and using information
+									while( $pages_assoc = mysqli_fetch_assoc($resource_pages) ){ 
 							?>
-				<ul class="pages">
-					<?php 		//3.b Fetching resource_pages and using information ?>
-					<?php 		while( $pages_assoc = mysqli_fetch_assoc($resource_pages) ){ ?>
-					<li> 			<a href="manage_content.php?page=<?php echo $pages_assoc["id"]?>"><?php echo "{$pages_assoc["menu_name"]}"; ?></a></li>
-					<?php 		} ?>
-				</ul>
-			</li>
-			<?php 	  } ?>
+										<?php
+											echo "<li ";
+											if( $selected_page_id == $pages_assoc["id"]){
+												echo "class=\"selected\"";
+											}
+											echo ">";
+										?>
+											<a href="manage_content.php?page=<?php echo $pages_assoc["id"]?>"><?php echo "{$pages_assoc["menu_name"]}"; ?></a>
+										</li>
+							<?php 	} ?>
+						</ul>
+					</li>
+		<?php 	} ?>
 		</ul>
 		<?php
 			// 4.a Free Result
@@ -47,8 +65,8 @@
 	<div class="page">
 		<h2>Manage Content</h2>
 		<?php
-			echo "Subject ID: {$subject_id}<br />";
-			echo "Page ID: {$page_id}";
+			echo "Subject ID: {$selected_subject_id}<br />";
+			echo "Page ID: {$selected_page_id}";
 		?>
 	</div> 
 </div>
